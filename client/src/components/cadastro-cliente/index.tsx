88997bad6532226d54ca235156e0a1daa3fa535a
Marcelo <marcelorcarnauba@gmail.com>
2024-09-22 19:34:54 -0300

    feat(client): Estilo e responsividade da pagina de clientes

--- client/src/components/cadastro-cliente/index.tsx
+++ client/src/components/cadastro-cliente/index.tsx
@@ -44,34 +44,38 @@ const CadastroClienteClient: React.FC<Props> = ({ }) => {
         })
 
     }
-    const [tipoDePessoa, setTipoDePessoa] = useState<string>("")
+    const [tipoDePessoa, setTipoDePessoa] = useState<string>("fisica")
 
     return (
-        <main className="bg-slate-100 flex items-center justify-center h-screen w-full">
-            <form onSubmit={handleSubmit(handleCreateCliente)} className="flex flex-col gap-2 bg-slate-50 px-20 py-10 rounded-md shadow-xl ">
-                <input type="text" {...register("nome_completo")} placeholder="Nome Completo" />
-                <input type="text" {...register("telefone")} placeholder="Telefone" />
-                <input type="text" {...register("email")} placeholder="Email" />
-                <label htmlFor="tipoDePessoa">Tipo de Pessoa:</label>
-                <select name="tipoDePessoa" id="tipoDePessoa" defaultValue={"fisica"} onChange={(e) => setTipoDePessoa(e.target.value)}>
-                    <option value="fisica">Física</option>
-                    <option value="juridica">Jurídica</option>
-                </select>
-                {tipoDePessoa == "fisica" ?
-                    <input type="text" {...register("cpf")} placeholder="CPF" /> :
+        <main className="bg-slate-200 flex items-center justify-center h-max md:h-screen w-full">
+            <section className="flex flex-col justify-between items-center gap-8 py-8 bg-slate-50 px-8 md:px-20 rounded-xl shadow-xl w-full md:w-3/4">
+                <h1 className="text-lg md:text-2xl font-medium">Cadastrar Cliente</h1>
+                <form onSubmit={handleSubmit(handleCreateCliente)} className="flex flex-col items-center gap-4 w-full">
+                    <input type="text" {...register("nome_completo")} placeholder="Nome Completo" />
+                    <input type="text" {...register("telefone")} placeholder="Telefone" />
+                    <input type="text" {...register("email")} placeholder="Email" />
+                    <label htmlFor="tipoDePessoa">Tipo de Pessoa:</label>
+                    <select name="tipoDePessoa" id="tipoDePessoa" defaultValue={"fisica"} onChange={(e) => setTipoDePessoa(e.target.value)}>
+                        <option value="fisica">Física</option>
+                        <option value="juridica">Jurídica</option>
+                    </select>
+                    {tipoDePessoa == "fisica" ?
+                        <input type="text" {...register("cpf")} placeholder="CPF" /> :
 
-                    <input type="text" {...register("cnpj")} placeholder="CNPJ" />
-                }
-                <input type="text" {...register("instagram")} placeholder="Instagram" />
-                <input type="text" {...register("site")} placeholder="Site" />
-                <input type="text" {...register("endereco")} placeholder="Endereço" />
-                <label htmlFor="aniversario">Aniversário:</label>
-                <input type="date" {...register("aniversario")} />
-                <input type="text" {...register("linkedin")} placeholder="LinkedIn" />
-                <input type="text" {...register("observacoes")} placeholder="Observações" />
+                        <input type="text" {...register("cnpj")} placeholder="CNPJ" />
+                    }
+                    <input type="text" {...register("instagram")} placeholder="Instagram" />
+                    <input type="text" {...register("site")} placeholder="Site" />
+                    <input type="text" {...register("endereco")} placeholder="Endereço" />
+                    <label htmlFor="aniversario">Aniversário:</label>
+                    <input type="date" {...register("aniversario")} />
+                    <input type="text" {...register("linkedin")} placeholder="LinkedIn" />
+                    <input type="text" {...register("observacoes")} placeholder="Observações" />
 
-                <button type="submit">Cadastrar</button>
-            </form>
+                    <button className="rounded-md py-2 px-4 bg-slate-200" type="submit">Cadastrar</button>
+                </form>
+
+            </section>
         </main>
     );
 };

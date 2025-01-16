b79ba72678b90743f8e7ea4d3debe2393a23e38c
Marcelo <marcelorcarnauba@gmail.com>
2025-01-16 10:40:07 -0300

    feat: Configuração do banco de dados na Filles.io

--- server/src/db/database.ts
+++ server/src/db/database.ts
@@ -15,7 +15,8 @@ class Database {
                 host: process.env.MYSQL_HOST,
                 user: process.env.MYSQL_USER,
                 password: process.env.MYSQL_PASSWORD,
-                database: process.env.MYSQL_DB
+                database: process.env.MYSQL_DB,
+                port: parseInt(process.env.MYSQL_PORT || '3030', 10)
             }
 
             this.pool = mysql.createPool(options)

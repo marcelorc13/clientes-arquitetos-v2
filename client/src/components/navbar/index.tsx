5f5f4f3d7a7a4bf910c034ee23138584bbdcba57
Marcelo <marcelorcarnauba@gmail.com>
2025-01-28 10:20:02 -0300

    style: Mudança de estilo do Navbar

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -7,8 +7,8 @@ interface Props {
 
 const Navbar: React.FC<Props> = ({ }) => {
     return (
-        <nav className='fixed hidden md:flex ps-1 py-1 h-screen  md:w-1/4 lg:w-1/5'>
-            <div className='w-full flex flex-col pt-2 gap-1 items-center text-slate-50 bg-slate-800 rounded-xl'>
+        <nav className='fixed hidden md:flex h-screen  md:w-1/4 lg:w-1/5 shadow-md shadow-slate-950'>
+            <div className='w-full flex flex-col pt-2 gap-1 items-center text-slate-50 bg-slate-800'>
                 <h1 className='text-2xl font-medium'>Metro A Metro</h1>
                 <ul className='flex flex-col items-center'>
                     <li><Link href={'/clientes'}>Clientes</Link></li>

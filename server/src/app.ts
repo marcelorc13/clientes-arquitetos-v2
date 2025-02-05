8a0a66db092f5c44811bb1744a46e38ae51ac475
Marcelo <marcelorcarnauba@gmail.com>
2025-02-05 08:13:55 -0300

    fix(server) Correção para mandar cookies para frontend 2

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,8 @@ app.use(cors({
     origin: process.env.FRONTEND_DOMAIN,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    exposedHeaders: ['set-cookie']
 }))
 
 app.use("/api", appRouter)
